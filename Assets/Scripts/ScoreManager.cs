36cfbcb115e33e1398f6e315039e5e111f798300
bestblue2002 <karndanai.tong@bumail.net>
2023-10-22 18:27:53 +0700

    Add Combo System
    Add combo in ScoreManager.cs

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -8,7 +8,10 @@ public class ScoreManager : MonoBehaviour
     //public AudioSource hitSFX;
     //public AudioSource missSFX;
     public TMPro.TextMeshPro scoreText;
-    static int currentScore;
+    public static int currentScore;
+    public static int currentMultiplier;
+    public static int multiplierTracker; 
+    public static  int[] multiplierThresholds = {2,4,6};
     static int perfectScore = 20;
     static int greatScore = 10;
     
@@ -16,20 +19,48 @@ public class ScoreManager : MonoBehaviour
     {
         Instance = this;
         currentScore = 0;
+        currentMultiplier = 1;
     }
     public static void Perfect()
     {
-        currentScore+= perfectScore;
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+                Debug.Log("combo increase");
+            }
+        }
+        
+        currentScore+= perfectScore * currentMultiplier;
         //Instance.hitSFX.Play();
     }
 
     public static void Great()
     {
-        currentScore += greatScore;
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+                Debug.Log("combo increase");
+            }
+        }
+        
+        currentScore += greatScore * currentMultiplier;
     }
     public static void Miss()
     {
+        currentMultiplier = 1;
+        multiplierTracker = 0;
         currentScore += 0;
+        Debug.Log("Reset Combo");
         //Instance.missSFX.Play();
     }
     private void Update()
